9ff55059429146c1d0ab0533a9288ed4bb3ce8e3
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-18 15:18:14 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,2 +1,17 @@
 class PigLatinizer
+  attr_reader :text
+
+  def initialize(text)
+    @text= text.downcase.split
+  end
+
+  def make_latin
+
+  end
+
 end
+
+
+"pig" = "igpay"
+"latin" = "atinlay"
+"banana" = "ananabay"
