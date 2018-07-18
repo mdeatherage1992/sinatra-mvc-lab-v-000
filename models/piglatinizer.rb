03ec0d26eb2ecb11dac8ccb65cbef22091c4f2d3
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-18 15:36:14 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,12 +2,13 @@ require "pry"
 class PigLatinizer
   attr_reader :words
 
-  def initialize
-    @words = words
-  end
-
-  def pigLatinize
-
+  def piglatinize(input)
+    converter = input.split('')
+    if converter.length == 1
+      piglatinize_word(input)
+    else
+      piglatinize_sentence
+    end
   end
 
 end
