d150fd692bda73e1d25b4b7e687ddf42cb179155
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-18 15:25:20 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,11 +1,2 @@
-# class App < Sinatra::Base
-#   get '/' do
-#     erb :index
-#   end
-#
-#   post '/' do
-#     @analyzed_text = TextAnalyzer.new(params[:user_text])
-#
-#     erb :results
-#   end
-# end
+class App < Sinatra::Base
+end
